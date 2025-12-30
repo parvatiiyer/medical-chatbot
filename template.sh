6b6cbe722ff7602d4c2164cd891c8127581f39e4
parvatiiyer <parvatiiyer2007@gmail.com>
2025-12-30 23:21:29 +0530

    Folder structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Create project directory structure
+mkdir -p src
+mkdir -p research
+
+# Create main application file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created."
